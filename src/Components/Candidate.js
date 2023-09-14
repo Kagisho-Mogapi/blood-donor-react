920f295a9724b4b58df581389f41bce838bf87f6
Kagisho Mogapi <mogapikagisho@gmail.com>
2023-09-14 21:35:30 +0200

    Data can be displayed on a table

--- src/Components/Candidate.js
+++ src/Components/Candidate.js
@@ -1,25 +1,61 @@
 import React,{useState, useEffect} from "react";
 import { connect } from "react-redux";
 import * as actions from "../Actions/Candidate";
-import { Grid } from "@material-ui/core";
+import { Grid, Paper,TableContainer, Table, TableHead, TableRow, TableCell, TableBody, withStyles } from "@material-ui/core";
 import CandidateForm from "./CandidateForm";
 
-const Candidate = (props) => {
+const styles = theme => ({
+    root:{
+        "& .MuiTableCell-head":{
+            fontSize: "1.2rem"
+        }
+    },
+    paper:{
+        margin: theme.spacing(2),
+        padding: theme.spacing(2)
+    }
+})
+
+const Candidate = ({classes,...props}) => {
     useEffect(()=>{
         props.fetchAllCandidates()
     },[])
     
 
     return (
-        <Grid container>
-            <Grid item xs={6}>
-                <CandidateForm/>
+        <Paper className={classes.paper} elevation={3}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <CandidateForm/>
+                </Grid>
+                <Grid item xs={6}>
+                    <TableContainer>
+                        <Table>
+                            <TableHead className={classes.root}>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>Mobile</TableCell>
+                                    <TableCell>Blood Group</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    props.candidateList.map((record,index) =>{
+                                        return (<TableRow key={index} hover>
+                                            <TableCell>{record.fullName}</TableCell>
+                                            <TableCell>{record.mobile}</TableCell>
+                                            <TableCell>{record.bloodGroup}</TableCell>
+                                        </TableRow>)
+                                    })
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+
+                </Grid>
             </Grid>
-            <Grid item xs={6}>
-                <div>List of candidates</div>
-            </Grid>
-        </Grid>
-        );
+        </Paper>
+    );
 }
 
 const mapStateToProps = state =>({
@@ -31,4 +67,4 @@ const mapActionToProps ={
 }
 
 
-export default connect(mapStateToProps, mapActionToProps) (Candidate);
+export default connect(mapStateToProps, mapActionToProps) (withStyles(styles)(Candidate));
--- src/Components/CandidateForm.js
+++ src/Components/CandidateForm.js
@@ -1,7 +1,110 @@
-import React from "react";
+import React,{useState} from "react";
+import { Grid, TextField, withStyles, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import useForm from "./useForm";
 
-const CandidateForm = (props) => {
-    return ( <div>Candidates</div> );
+const initFieldValues ={
+    fullName: '',
+    mobile: '',
+    email: '',
+    age: '',
+    bloodGroup: '',
+    address: '',
+}
+
+const styles = theme =>({
+    root:{
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            minWidth: 200,
+        }
+    },
+    formControl:{
+        margin: theme.spacing(1),
+        minWidth: 210,
+    }
+})
+
+const CandidateForm = ({classes, ...props}) => {
+
+    const{
+        values,
+        setValues,
+        handleInputChange
+    } = useForm(initFieldValues)
+
+    //Material-ui Select
+    const inputLabel = React.useRef(null);
+    const [labelWidth, setLabelWidth] = React.useState(0);
+    React.useEffect(() =>{
+        setLabelWidth(inputLabel.current.offsetWidth);
+    },[]);
+
+    return (
+        <form autoComeplete="off" noValidation className={classes.root}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <TextField 
+                        name="fullName" 
+                        variant="outlined" 
+                        label="Full Name"
+                        value={values.fullName}
+                        onChange={handleInputChange}
+                    />
+                    <TextField 
+                        name="Email" 
+                        variant="outlined" 
+                        label="email"
+                        value={values.Email}
+                        onChange={handleInputChange}
+                    />
+                    <FormControl variant="outlined" className={classes.formControl}>
+                        <InputLabel ref={inputLabel}>blood Group</InputLabel>
+                        <Select
+                            name="bloodGroup"
+                            value={values.bloodGroup}
+                            onChange={handleInputChange}
+                            labelWidth={labelWidth}
+
+                        >
+                            <MenuItem value="">Select Blood Group</MenuItem>
+                            <MenuItem value="A">A +</MenuItem>
+                            <MenuItem value="A-">A -</MenuItem>
+                            <MenuItem value="B">B +</MenuItem>
+                            <MenuItem value="B-">B -</MenuItem>
+                            <MenuItem value="AB">AB +</MenuItem>
+                            <MenuItem value="AB-">AB -</MenuItem>
+                            <MenuItem value="O">O +</MenuItem>
+                            <MenuItem value="O-">O -</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField 
+                        name="Mobile" 
+                        variant="outlined" 
+                        label="mobile"
+                        value={values.Mobile}
+                        onChange={handleInputChange}
+                    />
+                    <TextField 
+                        name="Address" 
+                        variant="outlined" 
+                        label="address"
+                        value={values.Address}
+                        onChange={handleInputChange}
+                    />
+                    <TextField 
+                        name="Age" 
+                        variant="outlined" 
+                        label="age"
+                        value={values.Age}
+                        onChange={handleInputChange}
+                    />
+
+                </Grid>
+            </Grid>
+        </form>
+    );
 }
  
-export default CandidateForm;
+export default withStyles(styles)(CandidateForm);
